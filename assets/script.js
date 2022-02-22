7579ede1fa598da88b35b2e2c6811e8e78145b96
freyaliesel <80221739+freyaliesel@users.noreply.github.com>
2022-02-22 14:25:01 -0600

    preliminary objects and functions

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,59 @@
-// event listener for button to start quiz
+// get access to the html objects
+var container = document.getElementById("container");
+var content = document.getElementById("content");
+var startBtn = document.getElementById("startquiz");
+
+// create some new html objects
+var h2El = document.createElement("h2");
+
+
+//testing outside button presses 
+
+const questions = [
+    {
+        question: "First Question",
+        optionOne: "Correct Answer",
+        optionTwo: "Incorrect Answer One",
+        optionThree: "Incorrect Answer Two",
+        optionFour: "Incorrect Answer Three",
+        correctAnswer: "optionOne",
+    },
+];
+
+console.log(questions);
+
+
 
 // function for the game that the button calls - starts timer and prompts the questions
+function startQuiz() {
+    console.log(`start the quiz`);
+
+    // prepare the page for the game
+    setGameState();
+
+    // call the timer
+    // start the game
+    showQuestions();
+}
+
+function setGameState() {
+    startBtn.parentNode.removeChild(startBtn);
+    content.textContent = "";
+}
 
 // quiz portion function - Do this first
+function showQuestions() {
+    console.log(`game`);
+
+   
+}
 
 // timer function - integrate after implementing the basic quiz
 
-// score tracking 
+// score tracking
+
+// event listener for button to start quiz
+startBtn.addEventListener("click", function () {
+    console.log(`button clicked`);
+    startQuiz();
+});
