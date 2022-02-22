3fee78155b204d1b5d1352f761570bc280b5c442
freyaliesel <80221739+freyaliesel@users.noreply.github.com>
2022-02-22 12:42:38 -0600

    pseudocode

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+// event listener for button to start quiz
+
+// function for the game that the button calls - starts timer and prompts the questions
+
+// quiz portion function - Do this first
+
+// timer function - integrate after implementing the basic quiz
+
+// score tracking 
