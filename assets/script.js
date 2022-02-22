fd0ea0ce8f7cc30905255e2be8b7007f67dadfff
freyaliesel <80221739+freyaliesel@users.noreply.github.com>
2022-02-22 15:43:48 -0600

    added event listener to question buttons

--- assets/script.js
+++ assets/script.js
@@ -3,33 +3,36 @@ var containerEl = document.getElementById("container");
 var contentEl = document.getElementById("content");
 var startBtn = document.getElementById("startquiz");
 
-console.log(document.body.children)
+console.log(document.body.children);
 
 // create some new html objects
-var btns = ["A", "B", "C", "D"]
+var btns = ["A", "B", "C", "D"];
 for (var btn of btns) {
     var btnEl = document.createElement("button");
     containerEl.appendChild(btnEl);
     btnEl.textContent = btn;
     btnEl.style.padding = ".25em, 1em";
     btnEl.style.margin = ".25em 0";
-    btnEl.style.display = "none";
+    btnEl.style.display = "none";  
 }
 console.log(document.body.children[1].children);
 
-// testing outside button presses 
+// testing outside button presses
 
 const questions = [
     {
         query: "First Question",
-        options: ["Correct Answer", "Incorrect Answer", "Wrong Answer", "Bad Answer"],
+        options: [
+            "Correct Answer",
+            "Incorrect Answer",
+            "Wrong Answer",
+            "Bad Answer",
+        ],
         answer: "Correct Answer",
     },
 ];
 
 console.log(questions);
-
-
 
 // sets game state, starts timer, and prompts the questions
 function startQuiz() {
@@ -44,29 +47,38 @@ function startQuiz() {
 }
 
 function setGameState() {
+    console.log(`setting game state`);
     // remove the start button
     startBtn.parentNode.removeChild(startBtn);
     // clear the paragraph contents
     contentEl.textContent = "testing";
     // add buttons for answers
+}
+
+
+function checkAnswer() {
+    console.log(`checking answer`);
 
 }
 
 // quiz portion function - Do this first
 function showQuestions() {
-    console.log(`game`);
+    console.log(`show questions`);
     var btnEls = document.querySelectorAll("button");
     console.log(btnEls);
 
+    // display the question
     for (i = 0; i < questions.length; i++) {
         contentEl.textContent = questions[i].query;
+        // display the options
         for (j = 0; j < btnEls.length; j++) {
             btnEls[j].textContent = questions[i].options[j];
             btnEls[j].style.display = "block";
+            btnEls[j].addEventListener("click", function () {
+                checkAnswer();
+            });
         }
     }
-
-   
 }
 
 // timer function - integrate after implementing the basic quiz
