e81a35a43f9d7e0fc97b9123f5ff0d52b1e567ae
freyaliesel <80221739+freyaliesel@users.noreply.github.com>
2022-02-25 10:32:17 -0600

    list styling

--- assets/highscores/script.js
+++ assets/highscores/script.js
@@ -3,31 +3,37 @@ var listEl = document.querySelector("ol");
 var backBtn = document.querySelector("#back-btn");
 var clrScoreBtn = document.querySelector("#clear-scores");
 
-if (highScores !== null){
-// sort the high scores so the greatest score value is at index 0
-highScores.sort((a, b) => (a.score < b.score ? 1 : b.score < a.score ? -1 : 0));
-console.log(highScores);
+if (highScores !== null) {
+    // sort the high scores so the greatest score value is at index 0
+    highScores.sort((a, b) =>
+        a.score < b.score ? 1 : b.score < a.score ? -1 : 0
+    );
+    console.log(highScores);
 
-displayScores();
-}
-else {
+    displayScores();
+} else {
     clrScoreBtn.style.display = "none";
-    document.querySelector("h1").insertAdjacentHTML('afterend', '<p>Take the quiz to see your score!</p>');
+    document
+        .querySelector("h1")
+        .insertAdjacentHTML(
+            "afterend",
+            "<p>Take the quiz to see your score!</p>"
+        );
 }
 
 function displayScores() {
     for (var score of highScores) {
         console.log(score);
+        var index = listEl.children.length;
         var li = document.createElement("li");
         listEl.appendChild(li);
         li.textContent = `${score.name} - ${score.score}`;
         li.style.textAlign = "left";
         li.style.padding = "5px";
         li.style.marginLeft = "35px";
+        index % 2 == 0 ? li.style.backgroundColor = '#F5EEF8' : li.style.backgroundColor = '#EBDEF0';
     }
 }
-
-
 
 backBtn.addEventListener("click", function () {
     console.log(`back to quiz`);
